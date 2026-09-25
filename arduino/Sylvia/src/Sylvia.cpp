383583693164190d5a34655d37ef3662bf705b6a
Kashi828 <217323527+Kashi828@users.noreply.github.com>
2026-09-25 22:23:15 +0530

    chore: align SDK firmware identity

--- arduino/Sylvia/src/Sylvia.cpp
+++ arduino/Sylvia/src/Sylvia.cpp
@@ -198,7 +198,7 @@ bool Sylvia::heartbeat(const char* firmware, double temperature, double battery)
   if (!_configured) return false;
 
   StaticJsonDocument<1024> body;
-  body["firmware"] = firmware ? firmware : "sylvia-arduino-0.53.2";
+  body["firmware"] = firmware ? firmware : "sylvia-arduino-0.53.3";
 
   if (!isnan(temperature)) body["temperature"] = temperature;
   if (!isnan(battery)) body["battery"] = battery;
