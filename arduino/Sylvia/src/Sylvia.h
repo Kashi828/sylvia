a4e34c9da03ca175f7ce708cba2da7336cdccb21
Kashi828 <217323527+Kashi828@users.noreply.github.com>
2026-09-25 22:19:27 +0530

    chore: align SDK version accessor

--- arduino/Sylvia/src/Sylvia.h
+++ arduino/Sylvia/src/Sylvia.h
@@ -17,7 +17,7 @@
 
 class Sylvia {
 public:
-  static const char* sdkVersion() { return "0.3.0-alpha.8"; }
+  static const char* sdkVersion() { return "0.53.1"; }
   using CommandHandler = bool (*)(JsonObjectConst payload);
 
   Sylvia();
